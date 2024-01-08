8e0b98f98258ff025ae750a40c060f2067271e1c
Ismael BERNARD <i.bernard@sciencesu-lyon.fr>
2024-01-08 16:53:21 +0100

    ajout ex06

---
+++ ex06/alphabet.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    void ft_print_alphabet(void)
+    {
+        liste = new List<int>() { a, b, c, d, e, f, g, h, i, k, l, m, n, o, p, q, r, s, t, u, v,w, x, y, z };
+
+    
+        for (int i = 26; i == 0; i-=1)
+        {
+            return int index = i
+        }
+    }
